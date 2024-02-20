61050fec0f9e692e7f76d26834bfabfe87af82eb
yusharif <yousufmohamedsharif@gmail.com>
2024-02-20 17:19:45 +0300

    test

--- Test/main.go
+++ Test/main.go
@@ -12,11 +12,17 @@ func UltimatePointOne(n ***int) {
 	***n = 1
 }
 
-func main() {
-	n := 0
-	a := &n
-	b := &a
-	UltimatePointOne(&b)
+func DivMod(a int, b int, div *int, mod *int) {
+	*div = a / b
+	*mod = a % b
+}
 
-	fmt.Println(n)
+func main() {
+	a := 13
+	b := 2
+	var div int
+	var mod int
+	DivMod(a, b, &div, &mod)
+	fmt.Println(div)
+	fmt.Println(mod)
 }
