5a7270bab38107d62dfb87d3ea517bced9276593
yusharif <yousufmohamedsharif@gmail.com>
2024-02-20 19:30:39 +0300

    test

--- Test/main.go
+++ Test/main.go
@@ -50,10 +50,16 @@ func Swap(a *int, b *int) {
 	*b = temp
 }
 
+func StrRev(str string) string {
+	byte_str := []rune(str)
+	for i, j := 0, len(byte_str)-1; i < j; i, j = i+1, j-1 {
+		byte_str[i], byte_str[j] = byte_str[j], byte_str[i]
+	}
+	return string(byte_str)
+}
+
 func main() {
-	a := 0
-	b := 1
-	Swap(&a, &b)
-	fmt.Println(a)
-	fmt.Println(b)
+	s := "Hello World!"
+	s = StrRev(s)
+	fmt.Println(s)
 }
