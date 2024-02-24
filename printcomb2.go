9480d9d008e6b6099f89e46b5c53e4d4b23075e3
Yusuf yusharif <yusufmohamedsharif@gmail.com>
2024-02-24 17:43:06 +0300

    qqq

--- printcomb2.go
+++ printcomb2.go
@@ -5,16 +5,16 @@ import (
 )
 
 func PrintComb2() {
-	for i := 1; i <= 9; i++ {
-		for j := 0; j <= 9; j++ {
-			for k := i; k <= 9; k++ {
-				for l := 0; l <= 9; l++ {
+	for i := '1'; i <= '9'; i++ {
+		for j := '0'; j <= '9'; j++ {
+			for k := i; k <= '9'; k++ {
+				for l := '0'; l <= '9'; l++ {
 					if i != k || j != l {
-						z01.PrintRune(rune(i) + '0')
-						z01.PrintRune(rune(j) + '0')
+						z01.PrintRune(i + '0')
+						z01.PrintRune(j + '0')
 						z01.PrintRune(' ')
-						z01.PrintRune(rune(k) + '0')
-						z01.PrintRune(rune(l) + '0')
+						z01.PrintRune(k + '0')
+						z01.PrintRune(l + '0')
 						z01.PrintRune(',')
 					}
 				}
