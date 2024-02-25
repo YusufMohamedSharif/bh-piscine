f2464c6a99ebf3f1a8addd6d612bba56e2640d0c
yusharif <yusufmohamedsharif@gmail.com>
2024-02-25 16:13:08 +0300

    qqq

--- Test/main.go
+++ Test/main.go
@@ -120,27 +120,7 @@ func IterativeFactorial(nb int) int {
 	}
 }
 
-func isPrime(n int) bool {
-	if n <= 1 {
-		return false
-	}
-	if n <= 3 {
-		return true
-	}
-	if n%2 == 0 || n%3 == 0 {
-		return false
-	}
-	i := 5
-	for i*i <= n {
-		if n%i == 0 || n%(i+2) == 0 {
-			return false
-		}
-		i += 6
-	}
-	return true
-}
-
 func main() {
-	fmt.Println(isPrime(1))
+	fmt.Println(1)
 	// piscine.PrintNbr(-9223372036854775808)
 }
