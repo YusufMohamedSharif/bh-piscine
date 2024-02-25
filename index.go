39d9a5a3a705dc17fdcfe5c740d55e62a7373b2f
yusharif <yusufmohamedsharif@gmail.com>
2024-02-25 20:41:23 +0300

    qqq

---
+++ index.go
@@ -0,0 +1,17 @@
+package piscine
+
+func Index(s string, toFind string) int {
+	for i := 0; i <= len(s)-len(toFind); i++ {
+		found := true
+		for j := 0; j < len(toFind); j++ {
+			if s[i+j] != toFind[j] {
+				found = false
+				break
+			}
+		}
+		if found {
+			return i
+		}
+	}
+	return -1
+}
