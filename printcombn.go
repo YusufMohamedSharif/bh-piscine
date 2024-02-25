e75c5cf63e0e064d6af947bc9b84b1d3c4a7a123
yusharif <yusufmohamedsharif@gmail.com>
2024-02-25 17:25:23 +0300

    qqq

--- printcombn.go
+++ printcombn.go
@@ -2,21 +2,26 @@ package piscine
 
 import "github.com/01-edu/z01"
 
-func printCombinations(n int, current []rune, start rune) {
-	if n == 0 {
-		for i, digit := range current {
-			if i > 0 {
-				z01.PrintRune(',')
-				z01.PrintRune(' ')
+func PrintCombN(n int) {
+	var printHelper func(int, []rune, rune)
+	printHelper = func(n int, current []rune, start rune) {
+		if n == 0 {
+			for i, digit := range current {
+				if i > 0 {
+					z01.PrintRune(',')
+					z01.PrintRune(' ')
+				}
+				z01.PrintRune(digit)
 			}
-			z01.PrintRune(digit)
+			z01.PrintRune('\n')
+			return
 		}
-		z01.PrintRune('\n')
-		return
+
+		for i := start; i <= '9'; i++ {
+			newCurrent := append(current, i)
+			printHelper(n-1, newCurrent, i+1)
+		}
 	}
 
-	for i := start; i <= '9'; i++ {
-		newCurrent := append(current, i)
-		printCombinations(n-1, newCurrent, i+1)
-	}
+	printHelper(n, []rune{}, '0')
 }
