ebf640284f56feda112c77630517c315faeb550f
yusharif <yusufmohamedsharif@gmail.com>
2024-02-25 20:08:21 +0300

    qqq

--- nrune.go
+++ nrune.go
@@ -1,11 +1,20 @@
 package piscine
 
+func NRune(s string, n int) rune {
+	arr := []rune(s)
+	if n <= len(arr) && n > 0 {
+		return arr[n-1]
+	} else {
+		return 0
+	}
+}
+
 /*func NRune(s string, n int) rune {
 	if n < 0 || n >= len(s) {
 		return 0
 	}
 	return []rune(s)[n]
-}*/
+}
 
 func NRune(s string, n int) rune {
 	if n > 0 && n <= len(s) {
@@ -13,4 +22,4 @@ func NRune(s string, n int) rune {
 	} else {
 		return 0
 	}
-}
+}*/
