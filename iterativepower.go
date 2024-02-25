8a1f215fc6f3447e76f993dc522728451e2441c8
yusharif <yusufmohamedsharif@gmail.com>
2024-02-25 11:59:22 +0300

    qqq

--- iterativepower.go
+++ iterativepower.go
@@ -6,6 +6,8 @@ func IterativePower(nb int, power int) int {
 		return 0
 	} else if power == 1 || power == 0 {
 		return nb
+	} else if power == 0 {
+		return 1
 	} else {
 		for i := 0; i < power; i++ {
 			result *= nb
