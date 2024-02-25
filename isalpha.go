27b19c40cdbb8cb1d7539e8161b4658a2f1d0cf4
yusharif <yusufmohamedsharif@gmail.com>
2024-02-25 21:17:43 +0300

    qqq

--- isalpha.go
+++ isalpha.go
@@ -6,7 +6,7 @@ func IsAlpha(s string) bool {
 		return true
 	}
 	for i := 0; i < len(arr); i++ {
-		if (arr[i] >= 'a' && arr[i] <= 'z') || (arr[i] >= 'A' && arr[i] <= 'Z') {
+		if (arr[i] >= 'a' && arr[i] <= 'z') || (arr[i] >= 'A' && arr[i] <= 'Z') || (arr[i] >= '0' && arr[i] <= '9') {
 		} else {
 			return false
 		}
