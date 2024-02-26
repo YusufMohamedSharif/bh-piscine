89eac8e9e5c5527d0b8c54415d1fd1aede52d328
yusharif <yusufmohamedsharif@gmail.com>
2024-02-26 16:33:36 +0300

    qqq

--- capitalize.go
+++ capitalize.go
@@ -1,24 +1,8 @@
 package piscine
 
-func Capitalize(s string) string {
+func CapitalizeWords(s string) string {
 	arr1 := []rune(s)
 	arr2 := []rune{}
-<<<<<<< HEAD
-	newWordFlag := false
-	for i := 0; i < len(arr1); i++ {
-		if (i == 0 || newWordFlag) && (arr1[i] >= 'a' && arr1[i] <= 'z') {
-			arr2 = append(arr2, arr1[i]-32)
-			newWordFlag = false
-		} else {
-			arr2 = append(arr2, arr1[i])
-		}
-		if arr1[i] < 'A' || (arr1[i] > 'Z' && arr1[i] < 'a') || arr1[i] > 'z' {
-			newWordFlag = true
-		}
-	}
-	return string(arr2)
-}
-=======
 	newWordFlag := true // Flag to indicate the start of a new word
 	for i := 0; i < len(arr1); i++ {
 		if newWordFlag && ((arr1[i] >= 'a' && arr1[i] <= 'z') || (arr1[i] >= 'A' && arr1[i] <= 'Z') || (arr1[i] >= '0' && arr1[i] <= '9')) {
@@ -41,4 +25,3 @@ func Capitalize(s string) string {
 	}
 	return string(arr2)
 }
->>>>>>> 549e37848adb88494c01f1265e065589c349d507
