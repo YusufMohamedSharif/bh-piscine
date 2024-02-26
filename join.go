787317079232d5c5eb460b91cdbe150ba163118a
yusharif <yusufmohamedsharif@gmail.com>
2024-02-26 17:26:44 +0300

    qqq

--- join.go
+++ join.go
@@ -4,7 +4,10 @@ func Join(strs []string, sep string) string {
 	result := ""
 	for i := 0; i < len(strs); i++ {
 		result += strs[i]
-		result += sep
+		if i != len(strs)-1 {
+			result += sep
+		}
+
 	}
 	return result
 }
