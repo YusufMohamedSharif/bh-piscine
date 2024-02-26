c6ef3bde3eea229402e6945f415371f0412c84d7
root <dev@01-edu.org>
2024-02-26 10:00:01 +0000

    Update 'capitalize.go'

--- capitalize.go
+++ capitalize.go
@@ -3,16 +3,16 @@ package piscine
 func Capitalize(s string) string {
 	arr1 := []rune(s)
 	arr2 := []rune{}
-	newWordFlag := false
+	newWordFlag := true // Set to true to capitalize the first letter of the string
 	for i := 0; i < len(arr1); i++ {
-		if (i == 0 || newWordFlag) && (arr1[i] >= 'a' && arr1[i] <= 'z') {
-			arr2 = append(arr2, arr1[i]-32)
+		if newWordFlag && (arr1[i] >= 'a' && arr1[i] <= 'z') {
+			arr2 = append(arr2, arr1[i]-32) // Convert to uppercase
+			newWordFlag = false
+		} else if (arr1[i] < 'a' || arr1[i] > 'z') && (arr1[i] < 'A' || arr1[i] > 'Z') {
+			newWordFlag = true // Found a non-letter character, next character is the start of a new word
+			arr2 = append(arr2, arr1[i]) // Add the non-letter character as it is
 		} else {
-			arr2 = append(arr2, arr1[i])
-		}
-		newWordFlag = false
-		if arr1[i] < 'A' || (arr1[i] > 'Z' && arr1[i] > 'a') || arr1[i] > 'z' {
-			newWordFlag = true
+			arr2 = append(arr2, arr1[i]) // Add the character as it is
 		}
 	}
 	return string(arr2)
