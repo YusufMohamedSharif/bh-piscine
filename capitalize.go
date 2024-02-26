a1b70ac2bf7352c74c5661fba975713478ae1e07
root <dev@01-edu.org>
2024-02-26 10:45:49 +0000

    Update 'capitalize.go'

--- capitalize.go
+++ capitalize.go
@@ -3,21 +3,18 @@ package piscine
 func Capitalize(s string) string {
 	arr1 := []rune(s)
 	arr2 := []rune{}
-	newWordFlag := true // Set to true to capitalize the first letter of the string
+	shouldCapitalize := true
 	for i := 0; i < len(arr1); i++ {
-		if newWordFlag && ((arr1[i] >= 'a' && arr1[i] <= 'z') || (arr1[i] >= 'A' && arr1[i] <= 'Z')) {
-			if arr1[i] >= 'a' && arr1[i] <= 'z' {
-				arr2 = append(arr2, arr1[i]-32) // Convert to uppercase
-			} else {
-				arr2 = append(arr2, arr1[i]) // Already uppercase, add as it is
-			}
-			newWordFlag = false
-		} else if !((arr1[i] >= 'a' && arr1[i] <= 'z') || (arr1[i] >= 'A' && arr1[i] <= 'Z')) {
-			newWordFlag = true // Found a non-alphanumeric character, next character is the start of a new word
-			arr2 = append(arr2, arr1[i]) // Add the non-alphanumeric character as it is
+		if shouldCapitalize && (arr1[i] >= 'a' && arr1[i] <= 'z') {
+			arr2 = append(arr2, arr1[i]-32) // Convert to uppercase
+			shouldCapitalize = false
+		} else if !shouldCapitalize && (arr1[i] >= 'A' && arr1[i] <= 'Z') {
+			arr2 = append(arr2, arr1[i]+32) // Convert to lowercase
 		} else {
-			arr2 = append(arr2, arr1[i]) // Add the character as it is (don't change case)
-			newWordFlag = false
+			arr2 = append(arr2, arr1[i]) // Add the character as it is
+		}
+		if !((arr1[i] >= 'a' && arr1[i] <= 'z') || (arr1[i] >= 'A' && arr1[i] <= 'Z')) {
+			shouldCapitalize = true
 		}
 	}
 	return string(arr2)
