cc7bef388627328e2dc420626794e7f09343f54e
yusharif <yusufmohamedsharif@gmail.com>
2024-02-27 20:47:14 +0300

    hghfD

--- isprime.go
+++ isprime.go
@@ -13,28 +13,8 @@ func IsPrime(n int) bool {
 
 	for i := 5; i*i <= n; i += 6 {
 		if n%i == 0 || n%(i+2) == 0 {
-			return true
+			return false
 		}
 	}
 	return true
 }
-
-/*func IsPrime(n int) bool {
-	if n <= 1 {
-		return false
-	}
-	if n <= 3 {
-		return true
-	}
-	if n%2 == 0 || n%3 == 0 {
-		return false
-	}
-	i := 5
-	for i*i <= n {
-		if n%i == 0 || n%(i+2) == 0 {
-			return false
-		}
-		i += 6
-	}
-	return true
-}*/
