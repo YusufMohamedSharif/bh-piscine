eabfa350f6eca475e75eaaff49cb01f50305137f
yusharif <yusufmohamedsharif@gmail.com>
2024-02-27 12:43:51 +0300

    QQQ

---
+++ nbrconvertalpha/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"os"
+)
+
+func main() {
+	args := os.Args[1:]
+
+	// Check if the --upper flag is present
+	upper := false
+	if len(args) > 0 && args[0] == "--upper" {
+		upper = true
+		args = args[1:]
+	}
+
+	for _, arg := range args {
+		if n, valid := stringToInt(arg); valid && n >= 1 && n <= 26 {
+			if upper {
+				printChar('A' + byte(n-1))
+			} else {
+				printChar('a' + byte(n-1))
+			}
+		} else {
+			printChar(' ')
+		}
+	}
+
+	println()
+}
+
+// stringToInt converts a string to an integer and returns true if successful, false otherwise
+func stringToInt(s string) (int, bool) {
+	num := 0
+	for _, digit := range s {
+		if digit < '0' || digit > '9' {
+			return 0, false
+		}
+		num = num*10 + int(digit-'0')
+	}
+	return num, true
+}
+
+// printChar prints a single character without using fmt package
+func printChar(c byte) {
+	_, _ = os.Stdout.Write([]byte{c})
+}
