53703d12e03f9b25574ca763e8ede791beba4149
yusharif <yusufmohamedsharif@gmail.com>
2024-03-03 20:38:33 +0300

    qust 8

---
+++ displayfile/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) == 1 {
+		fmt.Println("File name missing")
+	} else if len(os.Args) > 2 {
+		fmt.Println("Too many arguments")
+	} else {
+		filename := os.Args[1]
+		file, error := os.Open(filename)
+
+		if error != nil {
+			fmt.Println(error)
+		} else {
+			data := make([]byte, 20)
+			file.Read(data)
+			fmt.Println(string(data))
+			file.Close()
+		}
+	}
+}
