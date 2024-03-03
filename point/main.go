cc3fdb81079cd85e36af9bfff7fcac99bcaab281
yusharif <yusufmohamedsharif@gmail.com>
2024-03-03 19:45:00 +0300

    qust 8

--- point/main.go
+++ point/main.go
@@ -1,4 +1,6 @@
-package piscine
+package main
+
+import "github.com/01-edu/z01"
 
 type point struct {
 	x int
@@ -8,4 +10,26 @@ type point struct {
 func setPoint(ptr *point) {
 	ptr.x = 42
 	ptr.y = 21
+}
+
+func main() {
+	points := &point{}
+
+	setPoint(points)
+
+	xS := "x = "
+	yS := ", y = "
+
+	for _, a := range xS {
+		z01.PrintRune(a)
+	}
+	z01.PrintRune(52)
+	z01.PrintRune(50)
+
+	for _, b := range yS {
+		z01.PrintRune(b)
+	}
+	z01.PrintRune(50)
+	z01.PrintRune(49)
+	z01.PrintRune('\n')
 }
