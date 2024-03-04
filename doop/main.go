7343dcd94129df42ed06ea9a9e6c014fe50397b8
yusharif <yusufmohamedsharif@gmail.com>
2024-03-04 18:10:49 +0300

    qust 9

---
+++ doop/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	// Check if the number of arguments is correct
+	if len(os.Args) != 4 {
+		return
+	}
+
+	// Parse arguments
+	value1, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		return
+	}
+	operator := os.Args[2]
+	value2, err := strconv.Atoi(os.Args[3])
+	if err != nil {
+		return
+	}
+
+	// Perform operation based on the operator
+	var result int
+	switch operator {
+	case "+":
+		result = value1 + value2
+	case "-":
+		result = value1 - value2
+	case "/":
+		if value2 == 0 {
+			fmt.Println("No division by 0")
+			return
+		}
+		result = value1 / value2
+	case "*":
+		result = value1 * value2
+	case "%":
+		if value2 == 0 {
+			fmt.Println("No modulo by 0")
+			return
+		}
+		result = value1 % value2
+	default:
+		return
+	}
+
+	// Print the result
+	fmt.Println(result)
+}
