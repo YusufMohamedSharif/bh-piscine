1c937a5a169e3c4f8d341477ee333504c3387603
yusharif <yusufmohamedsharif@gmail.com>
2024-03-08 18:35:58 +0300

    Hackathon

--- abort.go
+++ abort.go
@@ -1,23 +1,26 @@
 package piscine
 
-import "sort"
-
 func Abort(a, b, c, d, e int) int {
-	// Store the numbers in a slice
+	// Bubble sort the numbers
 	numbers := []int{a, b, c, d, e}
+	for i := 0; i < 5; i++ {
+		for j := 0; j < 4-i; j++ {
+			if numbers[j] > numbers[j+1] {
+				// Swap the numbers if they're in the wrong order
+				numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
+			}
+		}
+	}
 
-	// Sort the numbers in ascending order
-	sort.Ints(numbers)
-
-	// Check if the number of elements is odd or even
-	n := len(numbers)
-	if n%2 == 1 {
+	// Calculate the median
+	middleIndex := 5 / 2
+	if 5%2 == 1 {
 		// If odd, return the middle number
-		return numbers[n/2]
+		return numbers[middleIndex]
 	} else {
 		// If even, return the average of the two middle numbers
-		middleLeft := numbers[n/2-1]
-		middleRight := numbers[n/2]
+		middleLeft := numbers[middleIndex-1]
+		middleRight := numbers[middleIndex]
 		return (middleLeft + middleRight) / 2.0
 	}
 }
