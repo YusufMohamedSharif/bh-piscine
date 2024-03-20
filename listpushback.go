d99434018b4727ad12aca984e4cf0265f385d0e9
yusharif <yusufmohamedsharif@gmail.com>
2024-03-20 16:09:42 +0300

    quest 11

---
+++ listpushback.go
@@ -0,0 +1,51 @@
+package piscine
+
+// function to insert element in the last position of the list
+func ListPushBack(l *List, data interface{}) {
+	n := &NodeL{Data: data}
+
+	if l.Head == nil {
+		l.Head = n
+		return
+	}
+
+	iterator := l.Head
+	for iterator.Next != nil {
+		iterator = iterator.Next
+	}
+	iterator.Next = n
+}
+
+// type node struct {
+// 	data int
+// 	next *node
+// }
+
+// func insert(head *node, data int) *node {
+// 	n := &node{data: data}
+
+// 	if head == nil {
+// 		return n
+// 	} else {
+// 		n.next = head
+// 		return n
+// 	}
+// }
+
+// func PrintList(head *node) {
+// 	for head != nil {
+// 		fmt.Print(head.data, " -> ")
+// 		head = head.next
+// 	}
+// 	fmt.Println(nil)
+// }
+
+// func main() {
+// 	var link *node
+
+// 	link = insert(link, 1)
+// 	link = insert(link, 2)
+// 	link = insert(link, 3)
+
+// 	PrintList(link)
+// }
