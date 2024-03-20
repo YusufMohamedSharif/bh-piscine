45587cc5cba4d51f4736c9562156cf90d4286f67
yusharif <yusufmohamedsharif@gmail.com>
2024-03-20 17:44:06 +0300

    quest 12

--- btreeapplybylevel.go
+++ btreeapplybylevel.go
@@ -1,31 +1,22 @@
 package piscine
 
-import "reflect"
+type Function func(string)
 
-func printOrderTraversal(root *TreeNode, niveau int, fn interface{}) {
+func printOrderTraversal(root *TreeNode, niveau int, fn Function) {
 	if root == nil {
 		return
 	}
 	if niveau == 1 {
-		Invoke(fn, root.Data)
+		fn(root.Data)
 	} else if niveau > 1 {
 		printOrderTraversal(root.Left, niveau-1, fn)
 		printOrderTraversal(root.Right, niveau-1, fn)
 	}
 }
 
-func BTreeApplyByLevel(root *TreeNode, fn interface{}) {
+func BTreeApplyByLevel(root *TreeNode, fn Function) {
 	h := BTreeLevelCount(root)
 	for i := 1; i <= h; i++ {
 		printOrderTraversal(root, i, fn)
 	}
-}
-
-func Invoke(fn interface{}, args ...string) {
-	v := reflect.ValueOf(fn)
-	rargs := make([]reflect.Value, len(args))
-	for i, a := range args {
-		rargs[i] = reflect.ValueOf(a)
-	}
-	v.Call(rargs)
 }
