6da99744466371da1d31653dbb7268cbb507cb9b
yusharif <yusufmohamedsharif@gmail.com>
2024-03-20 16:24:55 +0300

    quest 11

---
+++ listat.go
@@ -0,0 +1,22 @@
+package piscine
+
+func ListAt(l *NodeL, pos int) *NodeL {
+	index := 0
+	count := 0
+	head := l
+
+	for head != nil {
+		index++
+		head = head.Next
+	}
+	if pos <= index {
+		for l != nil {
+			if count == pos {
+				return l
+			}
+			count++
+			l = l.Next
+		}
+	}
+	return nil
+}
