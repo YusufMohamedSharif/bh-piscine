ad1e8c2adee42c9e53eac1bd589287198875028f
yusharif <yusufmohamedsharif@gmail.com>
2024-03-31 18:56:06 +0300

    Go-Reloaded

--- listforeachif.go
+++ listforeachif.go
@@ -1,5 +1,45 @@
 package piscine
 
+func IsAlNode(node *NodeL) bool {
+	switch node.Data.(type) {
+	case string, rune:
+		return true
+	case int, float32, float64, byte:
+		return false
+	}
+	return false
+}
+
+func IsPositive_node(node *NodeL) bool {
+	switch node.Data.(type) {
+	case int, float32, float64, byte:
+		return node.Data.(int) > 0
+	case string, rune:
+		return false
+	}
+	return false
+}
+
+func IsNegative_node(node *NodeL) bool {
+	switch node.Data.(type) {
+	case int, float32, float64, byte:
+		return node.Data.(int) > 0
+	case string, rune:
+		return false
+	}
+	return false
+}
+
+func IsNumeric_node(node *NodeL) bool {
+	switch node.Data.(type) {
+	case int, float32, float64, byte:
+		return true
+	case string, rune:
+		return false
+	}
+	return false
+}
+
 func ListForEachIf(l *List, f func(*NodeL), comp func(*NodeL) bool) {
 	it := l.Head
 	for it != nil {
