fe216622998330d841acf954d62a79addcc8e840
Yusuf Mohamed Sharif <ysharif@SCE.GOV.BH>
2024-04-03 12:59:50 +0300

    Ascii-Art

--- ascii-art/ascii-art.go
+++ ascii-art/ascii-art.go
@@ -65,29 +65,32 @@ func main() {
 			line8 += "        "
 
 		case '\\':
-			if arr[i+1] == 'n' {
-				if line1 != "" {
-					fmt.Println(line1)
-					fmt.Println(line2)
-					fmt.Println(line3)
-					fmt.Println(line4)
-					fmt.Println(line5)
-					fmt.Println(line6)
-					fmt.Println(line7)
-					fmt.Println(line8)
+			if arr[i] != arr[len(arr)-1] {
+				if arr[i+1] == 'n' {
+					if line1 != "" {
+						fmt.Println(line1)
+						fmt.Println(line2)
+						fmt.Println(line3)
+						fmt.Println(line4)
+						fmt.Println(line5)
+						fmt.Println(line6)
+						fmt.Println(line7)
+						fmt.Println(line8)
 
-					line1 = ""
-					line2 = ""
-					line3 = ""
-					line4 = ""
-					line5 = ""
-					line6 = ""
-					line7 = ""
-					line8 = ""
-					i++
-				} else {
-					fmt.Println()
-					i++
+						line1 = ""
+						line2 = ""
+						line3 = ""
+						line4 = ""
+						line5 = ""
+						line6 = ""
+						line7 = ""
+						line8 = ""
+						i++
+					} else {
+						fmt.Println()
+						i++
+					}
+
 				}
 
 			} else {
